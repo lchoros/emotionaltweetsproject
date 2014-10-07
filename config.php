31def8774b66465573a32cc0d6fd2014b6440018
lchoros <choros777@gmail.com>
2014-10-07 13:02:54 +0100

    Create simple search form and define config file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Configure your OAuth settings from your application at https://dev.twitter.com/apps
+
+define('TWITTER_CONSUMER_KEY', 'co2IWM83Ike8t8KlJKR6y4Buc');
+define('TWITTER_CONSUMER_SECRET', 'mPdzmQzOhVAj5CmNA8RpYXvC5EoBPRxnBa92MGkKilPXln0TZr');
+
+
+// Configure authentication credentials.
+// you can generate your own access key from the link above
+
+define('TWITTER_ACCESS_KEY', '2811285352-nK555d2Xxy8MeJH7qV4cx9WetJAkJtl9Ymqc9v4');
+define('TWITTER_ACCESS_SECRET', 'fJEtRfTsmfYEr1gGQzVosR25FwddVmNY6HjXluPj69OnC'); 
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Cache-Control" content="no-cache">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Lang" content="en">
+<title>Twitter Sentiment Analysis</title>
+</head>
+<body>
+<h1>Twitter Sentiment Analysis</h1>
+<p>Type your keyword below:</p>
+<form method="GET">
+    <label>Keyword: </label> <input type="text" name="q" /> 
+    <input type="submit" />
+</form>  
+</body>
+</html>
