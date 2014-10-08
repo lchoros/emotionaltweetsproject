a4658382f081cf3ec66e6d3ae4b2b21cc47e5333
lchoros <choros777@gmail.com>
2014-10-08 21:01:06 +0100

    Create basic layout

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
-// Configure your OAuth settings from your application at https://dev.twitter.com/apps
 
+// Configure Twitter authentication credentials.
 define('TWITTER_CONSUMER_KEY', 'co2IWM83Ike8t8KlJKR6y4Buc');
 define('TWITTER_CONSUMER_SECRET', 'mPdzmQzOhVAj5CmNA8RpYXvC5EoBPRxnBa92MGkKilPXln0TZr');
 
-
-// Configure authentication credentials.
-// you can generate your own access key from the link above
+// Configure Semantria authentication credentials.
 
 define('TWITTER_ACCESS_KEY', '2811285352-nK555d2Xxy8MeJH7qV4cx9WetJAkJtl9Ymqc9v4');
 define('TWITTER_ACCESS_SECRET', 'fJEtRfTsmfYEr1gGQzVosR25FwddVmNY6HjXluPj69OnC'); 
